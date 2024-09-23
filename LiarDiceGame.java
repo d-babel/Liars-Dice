a2ebcbe11c03989a4a39bb47a69f9cfc62a6f703
d-babel <drew.babel@menloschool.org>
2024-09-22 18:29:53 -0700

    .

--- LiarDiceGame.java
+++ LiarDiceGame.java
@@ -372,21 +372,10 @@ public class LiarDiceGame {
     }
 
     private void clearConsole() {
-        // try to clear the console
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            }
-        } catch (Exception e) {
-            // if console can't clear, print new lines
-            for (int i = 0; i < 50; i++) {
-                System.out.println();
-            }
-        }
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
+
     public static void main(String[] args) {
         LiarDiceGame game = new LiarDiceGame();
         game.startGame();
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
   - Fixed opponent difficulty errors
   - Implemented varying opponents into game & user input
   - Need to add "dummy cases" for invalid user input
+
+### 9/22
+  - Optimized clearConsole() as I didn’t understand what it really did from research code found online
+  
